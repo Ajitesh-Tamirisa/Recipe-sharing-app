6528f7e4c3a7d6f8835edfc9fb41d425a71aef93
Ajitesh <ajiteshsai@gmail.com>
2024-03-13 00:21:44 -0700

    Fetching reviews from API

--- frontend/recipePage.js
+++ frontend/recipePage.js
@@ -4,6 +4,7 @@ const recipeId = sessionStorage.getItem("recipeId");
 
 $(document).ready(function(){
     let recipeId = getRecipeIdFromUrl();
+    reviewList.innerHTML = "";
     console.log(recipeId);
     const requestOptions = {
         method: "GET",
@@ -19,6 +20,38 @@ $(document).ready(function(){
         document.getElementById("avgRating").innerHTML = `Average Rating: ${result.averageRating.toFixed(2)} (${result.ratingCount} ratings)`;
         document.getElementById("recipeImage").innerHTML = `<img src="${result.image1Url}" class="img-fluid rounded-circle" style="object-fit: cover; width: 350px; height: 350px;" alt="Image"><br/>`;
         document.getElementById("cookingTime").innerHTML = "Cooking Time: "+result.cookingTime+" minutes";
+    })
+    .catch((error) => console.error(error));
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const requestOptionsNew = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow"
+    };
+
+    fetch("http://localhost:8080/review/recipe/"+recipeId, requestOptionsNew)
+    .then((response) => response.json())
+    .then((result) => {
+        console.log(result);
+        let reviewList = document.getElementById("reviewList");
+        if(result.length === 0){
+            reviewList.innerHTML = "<p>No reviews yet.</p>";
+        }
+        else{
+            result.forEach(review => {
+                reviewList.innerHTML += `<li class="list-group-item my-2">
+                <div style="display: flex; align-items: center;">
+                    <img src="images/user.png" alt="User Profile Picture" class="mr-2" style="width: 40px; height: 40px; border-radius: 50%;">
+                    <h5 id="reviwerName">${review.reviewerName}</h5>
+                    <span id="rating" class="pt-2 mr-2 pr-2" style="position: absolute; top: 0; right: 0;">${Math.floor(review.rating)} stars</span>
+                </div><br/>                         
+                <p id="review">${review.review}</p>
+            </li>`
+            })
+        }
     })
     .catch((error) => console.error(error));
 })
